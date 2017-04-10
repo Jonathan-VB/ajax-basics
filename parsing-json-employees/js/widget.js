6bf816c3c74e666794ffdf5b1c2ace663cf008c8
Jonathan-VB <jonathan@viralbamboo.com>
2017-04-10 22:24:24 +0100

    Processing JSON Data
    - Now that we have a JSON response from the server and parsed it, we
    need to turn it into HTML and add it to the web page.

--- parsing-json-employees/js/widget.js
+++ parsing-json-employees/js/widget.js
@@ -4,7 +4,31 @@ var xhr = new XMLHttpRequest();
 // Step 2: Create callback function.
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
-    console.log(xhr.responseText);
+
+    // Convert the responseText string into a JavaScript object.
+    var employees = JSON.parse(xhr.responseText);
+
+    // Returns the type of the variable 'employees'.
+    // console.log(typeof employees);
+    console.log(employees);
+
+    var statusHTML = '<ul class="bulleted">';
+
+    // Employee loop.
+    for (var i = 0; i < employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+
+    // Select the id 'employeeList' and inject the code placed inside
+    // the variable 'statusHTML' to be displayed in the HTML.
+    document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
 
