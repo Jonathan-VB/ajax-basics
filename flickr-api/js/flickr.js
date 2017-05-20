5c7e599010cb8dd52eeaa619dec216007b872d11
Jonathan-VB <jonathan@viralbamboo.com>
2017-05-20 16:51:43 +0100

    Making the AJAX Request
    - Puttting together the required jQuery programming to make a JSONP
    request of the Flickr web server.

--- flickr-api/js/flickr.js
+++ flickr-api/js/flickr.js
@@ -3,5 +3,18 @@ $(document).ready(function() {
   $('button').click(function() {
     $("button").removeClass("selected");  // Removes the highlight from all buttons.
     $(this).addClass("selected");         // Adds the highlight to the clicked button.
+
+    var flickerAPI = "http://api.flickr.com/servies/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();  // Gets the text associated with the clicked button.
+    // 'tags' and 'format' are Flickr API query string params.
+    var flickrOptions = {
+          tags: animal,   // We are associating the tag with the text in the clicked button (ex: "Dog", "Cat" or "Moose").
+          format: "json", // Flickr sends XML by default. Change data to JSON format.
+    };
+    function displayPhotos(data) {
+
+    }
+    // Flickr AJAX request
+    $.getJSON(flickerAPI, flickerOptions, displayPhotos);
   }); // end click
 }); // end ready
